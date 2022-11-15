206de920f654530817ecf4ffb795ed0cc1af14d1
Akeel Lashley <akeellashley@gmail.com>
2022-11-14 20:51:27 -0500

    Implementing "AC",  "+/-", "%" functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishTypingNumber: Bool = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        isFinishTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number * 0.01)
+            }
+        }
+        
+    
+       
     
     }
 
@@ -24,7 +44,19 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+            
+        if let numValue  =  sender.currentTitle {
+            
+            if isFinishTypingNumber {
+                displayLabel.text = numValue
+                isFinishTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+
     }
 
 }
